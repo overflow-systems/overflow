0516345e682c20260c1304d8936b74b41941cf3e
Ismael Sousa <ismaelsousa136@hotmail.com>
2021-04-23 11:21:54 -0300

    Meta

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <meta name="description" content="Criação de sites, identidade visual, loja online, blog na baixada santista, faça um orçamento agora mesmo.">
+  <meta name="description" content="Criação de sites, identidade visual, loja online e blog, em Praia Grande, entre em contato e faça um orçamento agora mesmo.">
 
   <!-- Global site tag (gtag.js) - Google Analytics -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=G-FE1GRJV2FK"></script>
@@ -45,6 +45,6 @@
 
   <?php wp_head(); ?>
 
-  <title>Overflow Systems | Criação de Sites na Baixada Santista</title>
+  <title>Overflow Systems | Criação de Sites em Praia Grande</title>
 </head>
 <body>
