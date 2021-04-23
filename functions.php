5b53b85ccc777f4d20d8ffd98f9838ffa1a2d9e4
Ismael Sousa <ismaelsousa136@hotmail.com>
2021-04-22 22:18:32 -0300

    Admin logo / Fix css input

--- functions.php
+++ functions.php
@@ -10,5 +10,17 @@ function scripts() {
   wp_enqueue_script( 'main', get_template_directory_uri().'/assets/dist/js/main-min.js', ['jquery', 'mask'], null, true );
 }
 
+function my_login_logo() { ?>
+  <style type="text/css">
+      #login h1 a, .login h1 a {
+          background-image: url('https://overflow.dev.br/wp-content/themes/overflow/assets/dist/img/logo.svg');
+          background-size: contain;
+          width: auto;
+          height: 70px;
+      }
+  </style>
+<?php }
+
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
 add_action( 'wp_enqueue_scripts', 'styles' );
 add_action( 'wp_enqueue_scripts', 'scripts' );
