6e658d3f7a55f747be6285a27700e4115bd31fd2
Bruno Brasolin <50347981+BrunoBrasolin@users.noreply.github.com>
2021-04-28 16:27:40 -0300

    wordpress

--- index.php
+++ index.php
@@ -1,39 +1,17 @@
-<?php get_header(); ?>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-<div class="bg"></div>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-<main class="incoming">
-  <div class="content container">
-    <header class="header">
-      <h1>
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/logo.svg" alt="Overflow Systems - Criação de sites em Praia Grande" title="Overflow Systems - Criação de sites em Praia Grande" class="logo">
-      </h1>
-    </header>
-
-    <div class="left">
-      <h1 class="title">Site em <br> construção</h1>
-
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/building.svg" alt="Overflow Systems - Criação de sites em Praia Grande" title="Overflow Systems - Criação de sites em Praia Grande" class="building">
-    </div>
-
-    <div class="right">
-      <?= do_shortcode('[contact-form-7 id="29" title="Em construção html_class="form"]') ?>
-
-      <ul class="contact-list">
-        <li>
-          <a title="Entrar em contato via Whatsapp" href="http://api.whatsapp.com/send?phone=5513996706150&text=Olá, gostaria de fazer um orçamento" class="item"><img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/whatsapp.svg" alt="Overflow Systems - Criação de sites em Praia Grande" title="Overflow Systems - Criação de sites em Praia Grande" class="icon"> (13) 99670-6150</a>
-        </li>
-        <li>
-          <a title="Entrar em contato via Whatsapp" href="http://api.whatsapp.com/send?phone=5513981007944&text=Olá, gostaria de fazer um orçamento" class="item"><img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/whatsapp.svg" alt="Overflow Systems - Criação de sites em Praia Grande" title="Overflow Systems - Criação de sites em Praia Grande" class="icon"> (13) 98100-7944</a>
-        </li>
-        <li>
-          <a title="Entrar em contato via E-mail" href="mailto:atendimento@overflow.dev.br" class="item"><img src="<?php echo get_template_directory_uri(); ?>/assets/dist/img/email.svg" alt="Overflow Systems - Criação de sites em Praia Grande" title="Overflow Systems - Criação de sites em Praia Grande" class="icon">atendimento@overflow.dev.br</a>
-        </li>
-      </ul>
-    </div>
-    
-  </div>
-
-</main>
-
-<?php get_footer(); ?>
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
